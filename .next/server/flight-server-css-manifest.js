432ffb48cd04b90933b25fd150d3e36f442b64d7
Mariia Protsailo <108431414+MariiaPrtsl@users.noreply.github.com>
2023-04-24 13:35:11 +0000

    start project

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,18 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/workspaces/project-manager-tasks-nextjs/app/page.tsx": [
+      "/workspaces/project-manager-tasks-nextjs/app/page.module.css",
+      "/workspaces/project-manager-tasks-nextjs/node_modules/@next/font/google/target.css?{\"path\":\"app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "/workspaces/project-manager-tasks-nextjs/app/layout.tsx": [
+      "/workspaces/project-manager-tasks-nextjs/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/workspaces/project-manager-tasks-nextjs/app/page": [
+      "/workspaces/project-manager-tasks-nextjs/app/page.module.css",
+      "/workspaces/project-manager-tasks-nextjs/node_modules/@next/font/google/target.css?{\"path\":\"app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/workspaces/project-manager-tasks-nextjs/app/globals.css"
+    ]
+  }
+}
